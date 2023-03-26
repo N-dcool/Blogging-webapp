16164c5b0a447431c83cf2453d9ad57c9a8222fe
NARESH CHOUDHARY <nareshnc82643@gmail.com>
2023-03-26 22:16:36 +0530

    auth part completed ✔️ working

--- pages/enter.js
+++ pages/enter.js
@@ -35,6 +35,7 @@ function SignOutButton() {
     return <button onClick={() => auth.signOut()}>Sign Out</button>;
 }
 
+// Username form
 function UsernameForm() {
     const [formValue, setFormValue] = useState("");
     const [isValid, setIsValid] = useState(false);
@@ -42,20 +43,37 @@ function UsernameForm() {
 
     const { user, username } = useContext(UserContext);
 
-    useEffect(() => {
-        checkUsername(formValue);
-    }, [formValue]);
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        // Create refs for both documents
+        const userDoc = firestore.doc(`users/${user.uid}`);
+        const usernameDoc = firestore.doc(`usernames/${formValue}`);
+
+        // Commit both docs together as a batch write.
+        const batch = firestore.batch();
+        batch.set(userDoc, {
+            username: formValue,
+            photoURL: user.photoURL,
+            displayName: user.displayName,
+        });
+        batch.set(usernameDoc, { uid: user.uid });
+
+        await batch.commit();
+    };
 
     const onChange = (e) => {
         // Force form value typed in form to match correct format
         const val = e.target.value.toLowerCase();
         const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
         // Only set form value if length is < 3 OR it passes regex
         if (val.length < 3) {
             setFormValue(val);
             setLoading(false);
             setIsValid(false);
         }
+
         if (re.test(val)) {
             setFormValue(val);
             setLoading(true);
@@ -63,8 +81,14 @@ function UsernameForm() {
         }
     };
 
-    // Hit the firestore database to check if username is available
-    // If available, set isValid to true
+    //
+
+    useEffect(() => {
+        checkUsername(formValue);
+    }, [formValue]);
+
+    // Hit the database for username match after each debounced change
+    // useCallback is required for debounce to work
     const checkUsername = useCallback(
         debounce(async (username) => {
             if (username.length >= 3) {
@@ -78,34 +102,44 @@ function UsernameForm() {
         []
     );
 
-    const onSubmit = async (e) => {
-        "use strict";
+    return (
+        !username && (
+            <section>
+                <h3>Choose Username</h3>
+                <form onSubmit={onSubmit}>
+                    <input
+                        name="username"
+                        placeholder="myname"
+                        value={formValue}
+                        onChange={onChange}
+                    />
+                    <UsernameMessage username={formValue} isValid={isValid} loading={loading} />
+                    <button type="submit" className="btn-green" disabled={!isValid}>
+                        Choose
+                    </button>
 
-        return (
-            !username && (
-                <section>
-                    <h3>Choose Username</h3>
-                    <form onSubmit={onSubmit}>
-                        <input
-                            name="username"
-                            placeholder="username"
-                            value={formValue}
-                            onChange={onChange}
-                        />
-                        <button type="submit" className="btn-green" disabled={!isValid}>
-                            choose
-                        </button>
-                        <h3>Debug State</h3>
-                        <div>
-                            Username: {formValue}
-                            <br />
-                            Loading: {loading.toString()}
-                            <br />
-                            Username Valid: {isValid.toString()}
-                        </div>
-                    </form>
-                </section>
-            )
-        );
-    };
+                    <h3>Debug State</h3>
+                    <div>
+                        Username: {formValue}
+                        <br />
+                        Loading: {loading.toString()}
+                        <br />
+                        Username Valid: {isValid.toString()}
+                    </div>
+                </form>
+            </section>
+        )
+    );
+}
+
+function UsernameMessage({ username, isValid, loading }) {
+    if (loading) {
+        return <p>Checking...</p>;
+    } else if (isValid) {
+        return <p className="text-success">{username} is available!</p>;
+    } else if (username && !isValid) {
+        return <p className="text-danger">That username is taken!</p>;
+    } else {
+        return <p></p>;
+    }
 }
