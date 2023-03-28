a9f94f312fb9bc919c280b4a7bcc6f9c81d70303
NARESH CHOUDHARY <nareshnc82643@gmail.com>
2023-03-28 14:21:01 +0530

    ssr 404 error page ✔️ working

---
+++ components/PostContent.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import ReactMarkDown from "react-markdown";
+
+// UI component foe main post content
+export default function PostContent({ post }) {
+    const createdAt =
+        typeof post?.createdAt === "number" ? new Date(post.createdAt) : post.createdAt.toDate();
+
+    return (
+        <div className="card">
+            <h1>{post?.title}</h1>
+            <span className="text-sm">
+                Written by{" "}
+                <Link href={`/${post.username}`}>
+                    <a className="text-info">@{post.username}</a>
+                </Link>{" "}
+                on {createdAt.toISOString()}
+            </span>
+            <ReactMarkDown>{post?.content}</ReactMarkDown>
+        </div>
+    );
+}
--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,7 +1,65 @@
-export default function PostPage({}) {
+import { firestore, getUserWithUsername, postToJSON } from "@/lib/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import PostContent from "@/components/PostContent";
+
+export async function getServerSideProps({ params }) {
+    const { username, slug } = params;
+    const userDoc = await getUserWithUsername(username);
+
+    let post;
+    let path;
+
+    if (userDoc) {
+        const postRef = userDoc.ref.collection("posts").doc(slug);
+        post = postToJSON(await postRef.get());
+
+        path = postRef.path;
+    }
+
+    return {
+        props: { post, path },
+        revalidate: 5000,
+    };
+}
+
+export async function getStaticPaths() {
+    // Improve my usinf Admin SDK to select empty docs
+    const snapshot = await firestore.collectionGroup("posts").get();
+
+    const paths = snapshot.docs.map((doc) => {
+        const { slug, username } = doc.data();
+        return {
+            params: { username, slug },
+        };
+    });
+
+    return {
+        // must be in this format:
+        // paths: [
+        //     { params: { username, slug} }},
+        // ],
+        paths,
+        fallback: "blocking",
+    };
+}
+
+export default function Post(props) {
+    const postRef = firestore.doc(props.path);
+    const [realtimePost] = useDocumentData(postRef);
+
+    const post = realtimePost || props.post;
+
     return (
-        <main>
-            <h1>Post</h1>
+        <main className={styles.container}>
+            <section>
+                <PostContent post={post} />
+            </section>
+
+            <aside>
+                <p>
+                    <strong>{post.heartCount || 0} 💗</strong>
+                </p>
+            </aside>
         </main>
     );
 }
