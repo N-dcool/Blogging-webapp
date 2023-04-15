8c9bc4e54119e2f909d8801de409538b700683a0
NARESH CHOUDHARY <nareshnc82643@gmail.com>
2023-04-15 13:12:39 +0530

    ImageUpload working perfectly ✔️

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,10 +1,133 @@
-import Metatags from "@/components/Metatags";
+import styles from "@/styles/Admin.module.css";
+import AuthCheck from "@/components/AuthCheck";
+import { firestore, auth, serverTimestamp } from "@/lib/firebase";
+import ImageUploader from "@/components/ImageUploader";
+
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+import { useDocumentOnce } from "react-firebase-hooks/firestore";
+import { useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
+import Link from "next/link";
+import toast from "react-hot-toast";
 
 export default function AdminPostEdit({}) {
     return (
-        <main>
-            <Metatags title="admin-page" />
-            <h1>Admin Post Edit</h1>
+        <AuthCheck>
+            <PostManager />
+        </AuthCheck>
+    );
+}
+
+function PostManager() {
+    const [preview, setPreview] = useState(false);
+
+    const router = useRouter();
+    const { slug } = router.query;
+
+    const postRef = firestore
+        .collection("users")
+        .doc(auth.currentUser.uid)
+        .collection("posts")
+        .doc(slug);
+    const [post] = useDocumentOnce(postRef);
+
+    return (
+        <main className={styles.container}>
+            {post && (
+                <>
+                    <section>
+                        <h1>{post.data().title}</h1>
+                        <p>ID: {post.data().slug}</p>
+
+                        <PostForm defaultValues={post.data()} postRef={postRef} preview={preview} />
+                    </section>
+                    <aside>
+                        <h3>Tools</h3>
+                        <button onClick={() => setPreview(!preview)}>
+                            {preview ? "Edit" : "Preview"}
+                        </button>
+                        <Link href={`/${post.username}/${post.slug}`}>
+                            <button className="btn-blue">Live view</button>
+                        </Link>
+                        <DeletePostButton postRef={postRef} />
+                    </aside>
+                </>
+            )}
         </main>
+    );
+}
+function PostForm({ defaultValues, postRef, preview }) {
+    const { register, handleSubmit, reset, watch, formState } = useForm({
+        defaultValues,
+        mode: "onChange",
+    });
+    const { isValid, isDirty } = formState;
+
+    const updatePost = async ({ content, published }) => {
+        await postRef.update({
+            content,
+            published,
+            updatedAt: serverTimestamp(),
+        });
+        reset({ content, published });
+        toast.success("Post updated successfully!");
+    };
+
+    return (
+        <form onSubmit={handleSubmit(updatePost)}>
+            {preview && (
+                <div className="card">
+                    <ReactMarkdown>{watch("content")}</ReactMarkdown>
+                </div>
+            )}
+            <div className={preview ? styles.hidden : styles.controls}>
+                <ImageUploader />
+                <textarea
+                    name="content"
+                    {...register("content", {
+                        maxLength: { value: 2000, message: "content is too long" },
+                        minLength: { value: 10, message: "content is too short" },
+                        required: { value: true, message: "content is required" },
+                    })}
+                ></textarea>
+
+                {formState.errors.content && (
+                    <p className="text-danger">{formState.errors.content.message}</p>
+                )}
+                <fieldset>
+                    <input
+                        className={styles.checkbox}
+                        type="checkbox"
+                        name="published"
+                        {...register("Content")}
+                    />
+                    <label>Published</label>
+                </fieldset>
+                <button type="submit" className="btn-green" disabled={!isValid || !isDirty}>
+                    Save Changes
+                </button>
+            </div>
+        </form>
+    );
+}
+
+function DeletePostButton({ postRef }) {
+    const router = useRouter();
+
+    const deletePost = async () => {
+        const doIt = confirm("are you sure!");
+        if (doIt) {
+            await postRef.delete();
+            router.push("/admin");
+            toast("post annihilated ", { icon: "🗑️" });
+        }
+    };
+
+    return (
+        <button className="btn-red" onClick={deletePost}>
+            Delete
+        </button>
     );
 }
